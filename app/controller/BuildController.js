76958ecf7bd14e8c75a0e1187e4bf8f7bdee76a1
JDevlieghere <info@jonasdevlieghere.com>
2014-06-21 18:29:59 +0200

    Bugfix

--- app/controller/BuildController.js
+++ app/controller/BuildController.js
@@ -8,7 +8,7 @@ function BuildController(mongoose){
 
     this.get = function (req, res) {
         var Build = mongoose.model('Build', Schema.buildSchema);
-        Build.find({"owner": req.params.owner, "repo": req.params.repo}, {}, { sort: { 'created_at' : 1 } }, function(err, build){
+        Build.findOne({"owner": req.params.owner, "repo": req.params.repo}, {}, { sort: { 'created_at' : 1 } }, function(err, build){
             if(err) res.status(404).json({message: 'No build found'});
             else res.json(convert(build));
         });
