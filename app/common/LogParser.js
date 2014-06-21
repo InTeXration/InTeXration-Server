e3452c28093cc3b1865aa925000b1b11894517b1
JDevlieghere <info@jonasdevlieghere.com>
2014-06-22 00:06:00 +0200

    Minor update

--- app/common/LogParser.js
+++ app/common/LogParser.js
@@ -8,12 +8,13 @@ function LogParser(){
     var WARNINGS = 'Warning';
     var NEWLINE = '\n'
 
-    var find = function(lines, pattern){
+    var find = function(lines, pattern, replace){
         var results = [];
         var item = '';
         for (var i = 0; i < lines.length; i++) {
             var line = lines[i];
             if(item !== '' && line === ''){
+                if(replace) item = item.replace(pattern, '');
                 results.push(item);
                 item = '';
             }
@@ -26,14 +27,14 @@ function LogParser(){
 
     this.parse = function(path){
         var deferred = Q.defer();
-        fs.readFile(path, 'utf8', function (err, data){
+        fs.readFile(path, function (err, data){
             if(err){
                 logger.error('Log Parser: Cannot open file', {file:path});
                 deferred.reject(err);
             }else {
-                var lines = data.split(NEWLINE);
-                var errors = find(lines, ERROR);
-                var warnings = find(lines, WARNINGS);
+                var lines = data.toString().split(NEWLINE);
+                var errors = find(lines, ERROR, true);
+                var warnings = find(lines, WARNINGS, false);
                 deferred.resolve({
                     errors: errors,
                     warnings: warnings
