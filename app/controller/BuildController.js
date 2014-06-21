f7487e2e01c4058c55b331f23dc1a6f5e6b894c2
JDevlieghere <info@jonasdevlieghere.com>
2014-06-21 19:07:26 +0200

    Return newest build instead of oldest

--- app/controller/BuildController.js
+++ app/controller/BuildController.js
@@ -1,12 +1,11 @@
 var _ = require('underscore'),
-    Schema = require('./../Schema'),
-    CONFIG = require('config');
+    Schema = require('./../Schema');
 
 function BuildController(mongoose){
 
     this.get = function (req, res) {
         var Build = mongoose.model('Build', Schema.buildSchema);
-        Build.findOne({"blueprint.owner": req.params.owner, "blueprint.repo": req.params.repo}, {}, { sort: { 'created_at' : 1 } }, function(err, build){
+        Build.findOne({"blueprint.owner": req.params.owner, "blueprint.repo": req.params.repo}, {}, { sort: { 'created_at' : -1 } }, function(err, build){
             if(err || build === null) res.status(404).json({message: 'No build found'});
             else res.json(build);
         });
