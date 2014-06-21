a3b0b3a3e50c75cb62eac15d3de83bd61b9c7f44
JDevlieghere <info@jonasdevlieghere.com>
2014-06-21 18:34:50 +0200

    Forgot blueprint

--- app/controller/BuildController.js
+++ app/controller/BuildController.js
@@ -8,7 +8,7 @@ function BuildController(mongoose){
 
     this.get = function (req, res) {
         var Build = mongoose.model('Build', Schema.buildSchema);
-        Build.findOne({"owner": req.params.owner, "repo": req.params.repo}, function(err, build){
+        Build.findOne({"blueprint.owner": req.params.owner, "blueprint.repo": req.params.repo}, {}, { sort: { 'created_at' : 1 } }, function(err, build){
             if(err || build === null) res.status(404).json({message: 'No build found'});
             else res.json(convert(build));
         });
