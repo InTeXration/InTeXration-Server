480b5e7f12ec750586666f6ba76085604bb67a6b
JDevlieghere <info@jonasdevlieghere.com>
2014-06-26 22:48:40 +0200

    Updated ACAO Header

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.use(bodyParser.urlencoded());
 
 if (app.get('env') === 'development') {
     app.all('/', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
     });
 }
